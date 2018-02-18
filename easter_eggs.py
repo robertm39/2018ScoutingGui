c4ef861f5c9da1ba35fa89c6bdba45b8cb9e356b
Robert Morton <robertpmorton39@gmail.com>
2018-02-18 15:43:32 -0500

    Improved encryption

--- easter_eggs.py
+++ easter_eggs.py
@@ -5,11 +5,19 @@ Created on Sun Feb 18 14:13:41 2018
 @author: rober
 """
 
+import pickle as pkl
+
 import encode as ec
 #from code_getter import high
-high=5
-file = open('encoded.txt', 'r')
-file = list(file)
-file = ''.join(file)
-code = ec.decode(file, high=high)
+high=1100000
+
+file = open('encoded.txt', 'rb')
+code = pkl.load(file)
+#file = list(file)
+#file = ''.join(file)
+code = ec.decode(code, high=high)
+print(code)
+#print(type(code))
+#print(len(code))
+#print('print')
 exec(code)
